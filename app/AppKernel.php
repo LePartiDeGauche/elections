220dd84c2b2393274ed8a6906ae045d50e14a0e5
Naël Ferret <naelferret@gmail.com>
2014-05-03 21:49:11 +0200

    [ElectionsAdminBundle] add Sonata Admin
    Ajout et configuration de SonataAdminBundle.
    Activation du translator. Création de
    PartiDeGauche\ElectionsAdminBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,16 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            // SonataAdmin
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            // Nos bundle PG
             new PartiDeGauche\ElectionsBundle\PartiDeGaucheElectionsBundle(),
+            new PartiDeGauche\ElectionsAdminBundle\PartiDeGaucheElectionsAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/PartiDeGauche/ElectionsAdminBundle/PartiDeGaucheElectionsAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PartiDeGauche\ElectionsAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PartiDeGaucheElectionsAdminBundle extends Bundle
+{
+}
