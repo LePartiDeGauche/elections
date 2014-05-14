77b8bb90129183366b00ec90e8b3347af7ef2888
Naël Ferret <naelferret@gmail.com>
2014-05-14 23:16:10 +0200

    securisation /admin/

---
+++ src/PartiDeGauche/ElectionsBundle/Controller/LoginController.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file is part of the Parti de Gauche elections data project.
+ *
+ * The Parti de Gauche elections data project is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * The Parti de Gauche elections data project is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Parti de Gauche elections data project.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PartiDeGauche\ElectionsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     * @Template
+     */
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        );
+    }
+}
